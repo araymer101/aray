2033d662476c7bb5e7ac857306d05e37205fe60d
Jack Chatelain <jackchatelain@gmail.com>
2024-04-26 19:52:42 -0600

    Use \n for newline instead of print(" ")

--- pyball.py
+++ pyball.py
@@ -1,15 +1,13 @@
 import random
 
-responses = ["Yes.", "No.", "Ask again later.", "It is decidedly so.", "Outlook Good.", "Outlook not so good."]
+responses = ["Yes.", "No.", "Ask again later.", "It is decidedly so.", "Outlook good.", "Outlook not so good."]
 
 print(" __________")
 print("|    __    |")
 print("|   |__|   |")
 print("|   |__|   |")
 print("|__________|")
-print(" ")
-print("Hello, I am pyball. Ask me any yes or no question, and I will answer it for you. If you want to exit, type 'pyball exit'.")
-print(" ")
+print("\nHello, I am pyball. Ask me any yes or no question, and I will answer it for you. If you want to exit, type 'pyball exit'.\n")
 while True:
     user_input = input()
 
@@ -18,8 +16,6 @@ while True:
     else:
         number = random.randint(0, 5)
         result = responses[number]
-        print(" ")
-        print(result)
-        print(" ")
+        print("\nresult\n")
     print("What's your next question?")
 
